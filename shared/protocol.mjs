6bb657b2a8e2abed57b664b1daa0531bd3f538cc
Cauê Napier <cauenapierp@gmail.com>
2026-07-08 09:32:15 +0100

    fix: add PLUGIN message type to protocol definitions

--- shared/protocol.mjs
+++ shared/protocol.mjs
@@ -28,6 +28,7 @@ export const MSG = Object.freeze({
   MESSAGE_BOARD: "messageBoard",
   SCENE: "scene",
   CONNECTIONS: "connections",
+  PLUGIN: "plugin",
 });
 
 /** `action`-message gesture verbs. */
